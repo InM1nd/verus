1d6f26274bc8f5c1f6b9dd546bf3e138b08286f7
InM1nd <zabolotny1111@gmail.com>
2024-10-30 11:38:15 +0100

    fixes

--- js/swiper.js
+++ js/swiper.js
@@ -1,3 +1,12 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3.3,
+  slidesPerView: 2.3, // По умолчанию 3.3 слайда
+  breakpoints: {
+    1280: {
+      slidesPerView: 3.3, // При ширине экрана 1280px и больше - 2.3 слайда
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
